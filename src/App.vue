ad3ed2be8a07a79d1c3b5ac1d4f1ebf08065db04
qdrx <116974867+qdrx@users.noreply.github.com>
2023-11-30 23:20:03 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <NavBar/>
+  <div class="divider"></div>
+  <Terminal/>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+import Terminal from './components/TerminalEmulator.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    Terminal
+  }
+}
+</script>
+
+<style>
+@import './assets/styles.scss'; 
+
+.divider {
+  height: 2px;
+  background-color: #ff79c6;
+  margin: 20px auto;
+  width: 80%;
+}
+</style>
